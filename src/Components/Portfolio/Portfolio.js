9dd9c25c1985e069f09d88bd85fa7414bd3648f5
eduardo <lalo_page@hotmail.com>
2021-01-08 17:43:26 -0600

    feat: A JSON file was created. This  contains the project list. They were displayed in the Projects section using a Material-Ui Card

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
-
+/*Data */
+import Projects from '../../Data/projects.json';
 
 /*Components*/
 import ProjectItem from "../../Components/Projectitem/ProjectItem";
@@ -11,44 +12,27 @@ class Portfolio extends Component{
     constructor(props){
         super(props);
     }
+    componentDidMount(){
+        console.log(Projects);
+    }
 
     render() {
         const { classes } = this.props;
 
         return (
-            <Grid container className={classes.root} justify="center"  >
-            <Grid item xs={12}  md={4} sm={6} className={classes.itemCenter} style={{padding:10}}  >
-                <ProjectItem 
-                 alt="Benedetti's Online 2021"
-                 image="./images/projects/online2021/online2021_1.png"
-                 title="Benedetti's Online 2021"
-                 description="Tienda en línea de productos Benedetti's. Desarrollada con Laravel, Jquery, Bootstrap."
-                />
+            <Grid container className={classes.root} justify="left"  >
+                {Projects['Projects'].map(project=>
+                    <Grid item xs={12}  md={4} sm={6} className={classes.itemCenter} style={{padding:10}}  >
+                     <ProjectItem 
+                        alt={project.title}
+                        image={`./images/projects/${project.folder}/${project.image_name}`}
+                        title={`${project.title}`}
+                        description={`${project.description}`}
+                        />
+                    </Grid>
+                    )
+                }
             </Grid>
-            <Grid item xs={12}  md={4} sm={6} className={classes.itemCenter} style={{padding:10}}  >
-                <ProjectItem 
-                     alt="Benedetti's Online 2016"
-                     image="./images/projects/online2016/online2016_1.png"
-                     title="Benedetti's Online "
-                     description="Actual Tienda en línea de productos Benedetti's. Desarrollado con PHP, Jquery, HTML5, CSS3 y demás librerías."
-                
-                />
-            </Grid>
-            <Grid item xs={12}  md={4} sm={6} className={classes.itemCenter} style={{padding:10}}  >
-                <ProjectItem 
-                 alt="Benedetti's CASA"
-                 image="./images/projects/casa2020/casa2020_1.png"
-                 title="Benedetti's CASA "
-                 description="Centro de Atención y Solución al Asociado. Desarrollado con Ruby on Rails, React, Material UI."
-                
-                
-                />
-            </Grid>
-            
-           
-                
-            </Grid>
-
           );
       }
 }
