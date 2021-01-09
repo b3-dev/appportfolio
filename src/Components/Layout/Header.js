d82eb6b4f2d4743c6467e34977ed6320bd80937d
Eduardo PC <xcodering@hotmail.com>
2021-01-09 02:20:52 -0600

    Change background color in appbar

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -23,7 +23,7 @@ class Header extends Component{
         return (
             <div className={classes.root} >
             <header >
-                <AppBar position="fixed" color="secondary"  >
+                <AppBar position="fixed" color="primary"  >
                     <Toolbar>
                         <IconButton color="inherit" aria-label="Menu">
                             <ComputerIcon />
